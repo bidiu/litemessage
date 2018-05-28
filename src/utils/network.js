d3994ab3e527568f910ffbd808d6ba3ea3d79c23
bedew <sunhe1007@126.com>
2018-05-27 20:41:30 -0400

    added more utils

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,10 +1,15 @@
 const getRemoteAddress = (socket) => {
-  return socket._socket.remoteAddress;
+  return socket._socket.remoteAddress.replace(/^.*:/, '');
 };
 
 const getRemotePort = (socket) => {
   return socket._socket.remotePort;
 };
 
+const getSocketAddress = (socket) => {
+  return `${getRemoteAddress(socket)}:${getRemotePort(socket)}`;
+};
+
 exports.getRemoteAddress = getRemoteAddress;
 exports.getRemotePort = getRemotePort;
+exports.getSocketAddress = getSocketAddress;
