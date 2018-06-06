bb5007f71ef5cc1c3952823ee8b77ac3b554517c
bedew <sunhe1007@126.com>
2018-06-06 09:07:23 -0400

    added cancellation support to minning util

--- src/utils/litecrypto.js
+++ src/utils/litecrypto.js
@@ -1,6 +1,16 @@
 const { fork } = require('child_process');
 const crypto = require('crypto');
 const path = require('path');
+const Promise = require('bluebird');
+
+Promise.config({
+  // enable warnings
+  warnings: true,
+  // enable long stack traces
+  longStackTraces: true,
+  //enable cancellation
+  cancellation: true
+});
 
 const sha256 = (content, digest = 'hex') => {
   return crypto.createHash('sha256')
@@ -9,12 +19,12 @@ const sha256 = (content, digest = 'hex') => {
 };
 
 const mine = (content, difficulty) => 
-  new Promise((resolve, reject) => {
+  new Promise((resolve, reject, onCancel) => {
     if (typeof difficulty !== 'number') { reject(new Error('Invalid difficulty.')); }
     let cp = fork(path.join(__dirname, '../liteprotocol/mine.js'), [content, difficulty]);
 
     let timer = setTimeout(() => {
-      cp.removeAllListeners('message');
+      cp.removeAllListeners();
       cp.kill('SIGTERM');
       reject(new Error('Mining timeouts.'));
     }, 120000);
@@ -22,6 +32,12 @@ const mine = (content, difficulty) =>
     cp.on('message', (nonce) => {
       clearTimeout(timer);
       resolve(nonce);
+    });
+   
+    onCancel(() => {
+      clearTimeout(timer);
+      cp.removeAllListeners();
+      cp.kill('SIGTERM');
     });
   });
 
