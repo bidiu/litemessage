77f43019bca220f650a3511f79294cbc568b2ce6
bedew <sunhe1007@126.com>
2018-08-18 15:34:23 -0400

    chore

--- webpack.production.js
+++ webpack.production.js
@@ -4,15 +4,15 @@ const nodeConfig = require('./webpack.node');
 const browserConfig = require('./webpack.browser');
 
 module.exports = env =>
-	merge(
-		attachRevision(),
-		generateSourceMaps({ type: 'source-map' }),
-		{
-			mode: 'production',
-			context: __dirname,
-			entry: {
-				litemessage: './index.js'
-			},
-			...(env.BUILD_TARGET === 'node' ? nodeConfig : browserConfig)
-		}
-	);
+  merge(
+    attachRevision(),
+    generateSourceMaps({ type: 'source-map' }),
+    {
+      mode: 'production',
+      context: __dirname,
+      entry: {
+        litemessage: './index.js'
+      },
+      ...(env.BUILD_TARGET === 'node' ? nodeConfig : browserConfig)
+    }
+  );
